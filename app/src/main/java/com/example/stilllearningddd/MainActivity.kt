fa2478e372fdcb8e946b4cb932abc15d7c2c4b15
indianathe3rd <indiemenziwa@gmail.com>
2026-02-12 21:34:56 +0200

    I guess UI

--- app/src/main/java/com/example/stilllearningddd/MainActivity.kt
+++ app/src/main/java/com/example/stilllearningddd/MainActivity.kt
@@ -4,13 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.stilllearningddd.presentation.NoteViewModel
 import com.example.stilllearningddd.ui.theme.StillLearningDDDTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +38,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             StillLearningDDDTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    val viewModel: NoteViewModel = viewModels<NoteViewModel>().value
                     Greeting(
-                        name = "Android",
+                        viewModel,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +50,48 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun Greeting(viewModel: NoteViewModel, modifier: Modifier = Modifier) {
+    var text by remember { mutableStateOf("") }
+    LaunchedEffect(Unit) { viewModel.loadNotes() }
+    Column(
         modifier = modifier
-    )
-}
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            OutlinedTextField(
+                value = text,
+                onValueChange = { text = it },
+                label = { Text("Enter a note") },
+                modifier = Modifier.weight(1f)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Button(onClick = {
+                viewModel.addNote(text)
+                text = ""
+            }) {
+                Text("Add Note")
+            }
+            Spacer(modifier = Modifier.height(15.dp))
+        }
+        LazyColumn {
+            items(viewModel.notes) { note ->
+                Row(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(vertical = 4.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(note.text, modifier = Modifier.weight(1f))
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Button(onClick = { viewModel.deleteNote(note) }) {
+                        Text("Delete")
+                    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    StillLearningDDDTheme {
-        Greeting("Android")
+                }
+
+            }
+        }
     }
 }
+
--- app/src/main/java/com/example/stilllearningddd/domain/model/Note.kt
+++ app/src/main/java/com/example/stilllearningddd/domain/model/Note.kt
@@ -1,6 +1,6 @@
 package com.example.stilllearningddd.domain.model
 
-class Note (
+ data class Note (
     val id:Int,
     val text: String
     )
