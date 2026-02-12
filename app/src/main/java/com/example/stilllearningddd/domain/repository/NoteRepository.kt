46116ccbf227c1491c237dd44438a16502bb0d54
indianathe3rd <indiemenziwa@gmail.com>
2026-02-12 19:40:43 +0200

    repository

--- app/src/main/java/com/example/stilllearningddd/domain/repository/NoteRepository.kt
+++ app/src/main/java/com/example/stilllearningddd/domain/repository/NoteRepository.kt
@@ -1,4 +1,9 @@
 package com.example.stilllearningddd.domain.repository
 
+import com.example.stilllearningddd.domain.model.Note
+
 interface NoteRepository {
+    fun addNote(note: Note)
+    fun getNotes(): List<Note>
+    fun deleteNote(note: Note)
 }
